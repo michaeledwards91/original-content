38abe0dbbeea4b76bb78516ccb11bb396b610637
michaeledwards91 <michael.edwards1991@gmail.com>
2017-01-10 18:55:21 -0600

    More initial setup

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Dependencies
 var express = require("express");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
 
 // Requiring models
 var Comment = require("./models/Comment.js");
@@ -12,9 +14,20 @@ var cheerio = require("cheerio");
 
 // Express init
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+//Body Parser setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory "public"
 app.use(express.static("public"));
+
+// Handlebars setup
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set("view engine", "handlebars");
 
 // Database configuration with mongoose
 mongoose.connect("mongodb://localhost/newsdb");
@@ -30,3 +43,10 @@ db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
+// Require routes
+require("./routes/routes.js")(app);
+
+// Listen on port 3000
+app.listen(PORT, function() {
+	console.log("App listening on PORT " + PORT);
+});
